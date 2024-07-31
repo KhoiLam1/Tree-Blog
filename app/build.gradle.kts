0c8634486cb36fc4400edea64f9861359894f6a3
KhoiLam1 <lamminhkhoi0611@gmail.com>
2024-07-31 16:25:47 +0700

    Add package and versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,12 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-parcelize")
+    id("androidx.navigation.safeargs.kotlin")
+    id("com.google.devtools.ksp")
+    id("com.google.gms.google-services")
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("kotlin-kapt") // Add this line for Kapt
 }
 
 android {
@@ -9,12 +15,13 @@ android {
 
     defaultConfig {
         applicationId = "com.example.tree"
-        minSdk = 26
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        multiDexEnabled = true
     }
 
     buildTypes {
@@ -33,14 +40,98 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+        dataBinding = true
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.4.7" // Ensure the correct version is used
+    }
+
+    sourceSets {
+        getByName("main").java.srcDirs("build/generated/source/navigation-args")
+    }
+
+    dataBinding {
+        enable = true
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.kotlinx.serialization.json)
+
+    implementation(libs.firebase.storage)
+    implementation(libs.firebase.firestore.ktx)
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.database.ktx)
+    implementation(libs.firebase.auth)
+
+    implementation(libs.play.services.auth)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.activity.v190)
+    implementation(libs.androidx.coordinatorlayout)
+    implementation(libs.androidx.runtime.livedata)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    // Kotlin
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.navigation.compose)
+
+    // Import the BoM for the Firebase platform
+    implementation(platform(libs.firebase.bom))
+    // Declare the dependencies for the Firebase Cloud Firestore and Analytics libraries
+    implementation(libs.firebase.firestore)
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation(libs.glide)
+    kapt(libs.compiler) // Ensure you are using kapt for Glide
+
+    // Activity KTX
+    implementation(libs.androidx.activity.ktx)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.kotlinx.coroutines.core)
+
+    //
+    implementation(libs.retrofit)
+
+    // Compose
+    implementation(libs.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.ui)
+    implementation(libs.ui.graphics)
+    implementation(libs.ui.tooling.preview)
+    implementation(libs.material3)
+    implementation(libs.material.icons.extended)
+    implementation(libs.constraintlayout.compose)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.ui.test.junit4)
+    debugImplementation(libs.ui.tooling)
+    debugImplementation(libs.ui.test.manifest)
+    implementation(libs.glide)
+
+    // Coil for Jetpack Compose
+    implementation("io.coil-kt:coil-compose:2.0.0")
+    // Hilt dependencies
+    implementation("com.google.dagger:hilt-android:2.38.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.38.1")
+
+    // Hilt Jetpack Compose integration
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0-alpha03")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+
+    implementation("com.google.accompanist:accompanist-insets:0.21.1-beta")
+
 }
